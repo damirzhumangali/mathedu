fdb70052f92584d3f5310d3f208cb0f559665538
Damir Zhumangali <damirzhumangali125@gmail.com>
2026-06-07 19:00:48 +0500

    add videos via LFS

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'katex/dist/katex.min.css'
 import MathEdu from '../MathEdu'
 
 createRoot(document.getElementById('root')!).render(
